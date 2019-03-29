25cc4e5b1a29872f745b163d93154c7a0bfdd6b0
EliteFan <fan552426811@outlook.com>
2019-03-29 15:02:24 +0800

    cors

---
+++ main.js
@@ -0,0 +1,195 @@
+window.onload = function () {
+    // form提交，转移到iframe刷新
+    formSub.onclick = function () {
+        setTimeout(function () {
+            let iframe = window.frames['result'].document
+            let result = iframe.body.innerText
+            if (result === 'success') {
+                amount.innerText = amount.innerText - 1
+            }
+        }, 1000)
+    }
+    // 利用图片的 get 请求
+    btnImg.onclick = function () {
+        let image = document.createElement('img')
+        image.src = '/payImage'
+        image.onload = function () {
+            alert('image方法打钱成功')
+            amount.innerText = amount.innerText - 1
+        }
+        image.onerror = function () {
+            alert('image方法打钱失败')
+        }
+    }
+    // 利用 script 的 get 请求(srj方案)
+    btnScript.onclick = function () {
+        let script = document.createElement('script')
+        script.src = '/payScript'
+        document.body.appendChild(script)
+        script.onload = function (event) {
+            event.currentTarget.remove()
+        }
+        script.onerror = function () {
+            alert('script方法打钱失败(此消息发自网页本身)')
+            event.currentTarget.remove()
+        }
+    }
+    // 利用 JSONP 的 get 请求 alpha测试版 (可以跨域)
+    function callFunction(result) {
+        alert('这是调用的客户端 callback 函数,剩余存款为：' + result)
+    }
+    btnJsonpAlpha.onclick = function () {
+        let script = document.createElement('script')
+        script.src = 'http://server.com:8888/payJsonpAlpha?callback=callFunction'
+        document.body.appendChild(script)
+        script.onload = function (event) {
+            event.currentTarget.remove()
+        }
+        script.onerror = function () {
+            alert('script方法打钱失败(此消息发自网页本身)')
+            event.currentTarget.remove()
+        }
+    }
+    // 利用 JSONP 的 get 请求 beta测试版 (可以跨域)
+    btnJsonpBeta.onclick = function () {
+        let script = document.createElement('script')
+        let funcName = 'fpc' + parseInt(Math.random() * 10000)
+
+        window[funcName] = function (result) {
+            alert('这是调用的客户端 callback 函数,剩余存款为：' + result)
+        }
+        script.src = 'http://server.com:8888/payJsonpBeta?callback=' + funcName
+        document.body.appendChild(script)
+        script.onload = function (event) {
+            event.currentTarget.remove()
+        }
+        script.onerror = function () {
+            alert('script方法打钱失败(此消息发自网页本身)')
+            event.currentTarget.remove()
+        }
+    }
+    // 利用 jQuery 中的 JSONP (可以跨域)
+    btnJqueryJsonp.onclick = function () {
+        $.ajax({
+            url: 'http://server.com:8888/payJquery',
+            dataType: 'jsonp',
+            success: function (result) {
+                alert('这是调用的客户端 callback 函数,剩余存款为：' + result)
+            }
+        })
+    }
+    // 利用 AJAX 的 XMLHttpRequest 请求一段 XML (不能跨域)
+    btnAJAXXML.onclick = function () {
+        let request = new XMLHttpRequest()
+        request.open('post', 'http://client.com:7777/AJAXXML')
+        request.send()
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    let string = request.responseText
+                    console.log('请求成功' + string)
+                    let parser = new DOMParser()
+                    let xmlDom = parser.parseFromString(string, 'text/xml')
+                    let from = xmlDom.getElementsByTagName('from')[0].textContent
+                    console.log(from)
+                } else {
+                    console.log('请求失败')
+                }
+            }
+        }
+    }
+    // 利用 AJAX 的 XMLHttpRequest 请求一段 JSON (不能跨域)
+    btnAJAXJSON.onclick = function () {
+        let request = new XMLHttpRequest()
+        // request.open('post', 'http://client.com:7777/AJAXJSON')
+        request.open('post', 'http://server.com:8888/AJAXJSON')
+        request.send()
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    let string = request.responseText
+                    console.log('请求成功' + string)
+                    let obj = window.JSON.parse(string)
+                    console.log(obj.note.body)
+                } else {
+                    console.log('请求失败')
+                }
+            }
+        }
+    }
+    // 利用 AJAX 的 XMLHttpRequest 尝试跨域请求一段 JSON (AJAX并不能跨域请求)
+    // 响应会成功，但是并不能操作返回的数据
+    btnAJAXJSONCross.onclick = function () {
+        let request = new XMLHttpRequest()
+        request.onreadystatechange = function () {
+            console.log('readyState 为：' + request.readyState)
+            if (request.readyState === 4) {
+                console.log('status 为：' + request.status)
+                if (request.status >= 200 && request.status < 300) {
+                    let string = request.responseText
+                    console.log('请求成功' + string)
+                    let obj = window.JSON.parse(string)
+                    console.log(obj.note.body)
+                } else {
+                    console.log('请求失败')
+                }
+            }
+        }
+        request.open('post', 'http://server.com:8888/AJAXJSONCross')
+        request.send()
+    }
+    // 利用 AJAX CORES 尝试跨域请求一段 JSON
+    btnAJAXJSONCrossCORS.onclick = function () {
+        let request = new XMLHttpRequest()
+        request.onreadystatechange = function () {
+            console.log('readyState 为：' + request.readyState)
+            if (request.readyState === 4) {
+                console.log('status 为：' + request.status)
+                if (request.status >= 200 && request.status < 300) {
+                    let string = request.responseText
+                    console.log('请求成功' + string)
+                    let obj = window.JSON.parse(string)
+                    console.log(obj.note.body)
+                } else {
+                    console.log('请求失败')
+                }
+            }
+        }
+        request.open('post', 'http://server.com:8888/AJAXJSONCrossCORS')
+        request.send()
+    }
+    btnJquerytest.onclick = function () {
+        let script = document.createElement('script')
+        script.src = 'https://testapi.internet.bs/Domain/Check?ApiKey=testapi&Password=testpass&Domain=example.com'
+        document.body.appendChild(script)
+        script.onload = function (event) {
+            console.log(event.currentTarget)
+        }
+        script.onerror = function () {
+            alert('失败')
+        }
+
+        // let request = new XMLHttpRequest()
+        // request.open('post','https://testapi.internet.bs/Domain/Check?ApiKey=testapi&Password=testpass&Domain=example.com')
+        // request.send()
+        // request.onreadystatechange = function(){
+        //     if(request.readyState === 4){
+        //         if(request.status >= 200 && request.status < 300){
+        //             console.log('请求成功')
+        //             let string = request.responseText
+        //             console.log(string)
+        //         }else{
+        //             console.log('请求失败')
+        //         }
+        //     }
+        // }
+
+        // $.ajax({
+        //     url: 'https://testapi.internet.bs/Domain/Check?ApiKey=testapi&Password=testpass&Domain=example.com',
+        //     dataType: 'plain',
+        //     success: function(result){
+        //         console.log(result)
+        //     }
+        // })
+    }
+}
--- server.js
+++ server.js
@@ -4,125 +4,196 @@ var fs = require('fs')
 var url = require('url')
 
 var port = process.env.port || 7777;
-var server = http.createServer(function(request,response){
+var server = http.createServer(function (request, response) {
 
-    var temp = url.parse(request.url,true)
+    var temp = url.parse(request.url, true)
     var path = temp.pathname
     var query = temp.query
     var method = request.method
 
-    if(path === '/'){
-        var string = fs.readFileSync('./index.html','utf-8')
-        var amount = fs.readFileSync('./db','utf-8')
-        string = string.replace('&&&amount&&&',amount)
-        response.setHeader('Content-Type','text/html;charset=utf-8')
+    if (path === '/') {
+        var string = fs.readFileSync('./index.html', 'utf-8')
+        var amount = fs.readFileSync('./db', 'utf-8')
+        string = string.replace('&&&amount&&&', amount)
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(string)
         response.end()
-    }else if(path === '/style.css'){
-        var string = fs.readFileSync('./style.css','utf-8')
-        response.setHeader('Content-Type','text/css')
+    } else if (path === '/style.css') {
+        var string = fs.readFileSync('./style.css', 'utf-8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/css')
         response.write(string)
         response.end()
-    }else if(path === '/main.js'){
-        var string = fs.readFileSync('./main.js','utf-8')
-        response.setHeader('Content-Type','text/javascript')
+    } else if (path === '/main.js') {
+        var string = fs.readFileSync('./main.js', 'utf-8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/javascript')
         response.write(string)
         response.end()
-    }else if(path === '/payForm' && method.toUpperCase() === 'POST'){
-        var amount = fs.readFileSync('./db','utf-8')
-        if(Math.random()>0.5){
+    } else if (path === '/payForm' && method.toUpperCase() === 'POST') {
+        var amount = fs.readFileSync('./db', 'utf-8')
+        if (Math.random() > 0.5) {
             amount = amount - 1
-            fs.writeFileSync('./db',amount)
+            fs.writeFileSync('./db', amount)
             response.write('success')
-        }else{
+        } else {
+            response.statusCode = 400
             response.write('failed')
         }
         response.end()
-    }else if(path === '/payImage'){
-        var amount = fs.readFileSync('./db','utf-8')
-        if(Math.random()>0.5){
-            response.setHeader('Content-Type','image/png')
+    } else if (path === '/payImage') {
+        var amount = fs.readFileSync('./db', 'utf-8')
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'image/png')
             response.statusCode = 200
             amount = amount - 1
-            fs.writeFileSync('./db',amount)
+            fs.writeFileSync('./db', amount)
             response.write(fs.readFileSync('./success.png'))
             // response.write('打钱成功')
-        }else{
+        } else {
             response.statusCode = 400
             // response.write('打钱失败')
         }
         response.end()
-    }else if(path === '/payScript'){
-        var amount = fs.readFileSync('./db','utf-8')
-        if(Math.random()>0.5){
-            response.setHeader('Content-Type','text/javascript')
+    } else if (path === '/payScript') {
+        var amount = fs.readFileSync('./db', 'utf-8')
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/javascript')
             response.statusCode = 200
             amount = amount - 1
-            fs.writeFileSync('./db',amount)
+            fs.writeFileSync('./db', amount)
             response.write('alert("script方法打钱成功(此消息发自服务器)");amount.innerText = amount.innerText - 1')
             // response.write('打钱成功')
-        }else{
+        } else {
             response.statusCode = 400
             // response.write('打钱失败')
         }
         response.end()
-    }else if(path === '/payJsonpAlpha'){
-        var amount = fs.readFileSync('./db','utf-8')
-        if(Math.random()>0.5){
-            response.setHeader('Content-Type','text/javascript')
+    } else if (path === '/payJsonpAlpha') {
+        var amount = fs.readFileSync('./db', 'utf-8')
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/javascript')
             response.statusCode = 200
             amount = amount - 1
-            fs.writeFileSync('./db',amount)
+            fs.writeFileSync('./db', amount)
             response.write(`
             alert("script方法打钱成功(此消息发自服务器)")
             amount.innerText = amount.innerText - 1
             ${query.callback}.call(undefined,${amount})
             `)
             // response.write('打钱成功')
-        }else{
+        } else {
             response.statusCode = 400
             // response.write('打钱失败')
         }
         response.end()
-    }else if(path === '/payJsonpBeta'){
-        var amount = fs.readFileSync('./db','utf-8')
-        if(Math.random()>0.5){
-            response.setHeader('Content-Type','text/javascript')
+    } else if (path === '/payJsonpBeta') {
+        var amount = fs.readFileSync('./db', 'utf-8')
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/javascript')
             response.statusCode = 200
             amount = amount - 1
-            fs.writeFileSync('./db',amount)
+            fs.writeFileSync('./db', amount)
             response.write(`
             alert("script方法打钱成功(此消息发自服务器)")
             amount.innerText = amount.innerText - 1
             ${query.callback}.call(undefined,${amount})
             `)
             // response.write('打钱成功')
-        }else{
+        } else {
             response.statusCode = 400
             // response.write('打钱失败')
         }
         response.end()
-    }else if(path === '/payJquery'){
-        var amount = fs.readFileSync('./db','utf-8')
-        if(Math.random()>0.5){
-            response.setHeader('Content-Type','text/javascript')
+    } else if (path === '/payJquery') {
+        var amount = fs.readFileSync('./db', 'utf-8')
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/javascript')
             response.statusCode = 200
             amount = amount - 1
-            fs.writeFileSync('./db',amount)
+            fs.writeFileSync('./db', amount)
             response.write(`
             alert("JqueryScript方法打钱成功(此消息发自服务器)")
             amount.innerText = amount.innerText - 1
             ${query.callback}.call(undefined,${amount})
             `)
             // response.write('打钱成功')
-        }else{
+        } else {
             response.statusCode = 400
             // response.write('打钱失败')
         }
         response.end()
-    }else{
+    } else if (path === '/AJAXXML') {
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/xml')
+            response.statusCode = 200
+            response.write(`
+            <note>
+                <from>fpc</from>
+                <to>syq</to>
+                <body>i love you</body>
+            </note>
+            `)
+        } else {
+            response.statusCode = 400
+        }
+        response.end()
+    } else if (path === '/AJAXJSON') {
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/json')
+            response.statusCode = 200
+            response.write(`
+            {
+                "note":{
+                    "from":"fpc",
+                    "to":"syq",
+                    "body":"i love you"
+                }
+            }
+            `)
+        } else {
+            response.statusCode = 400
+        }
+        response.end()
+    } else if (path === '/AJAXJSONCross') {
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/json')
+            response.statusCode = 200
+            response.write(`
+            {
+                "note":{
+                    "from":"fpc",
+                    "to":"syq",
+                    "body":"i love you"
+                }
+            }
+            `)
+        } else {
+            response.statusCode = 400
+        }
+        response.end()
+    } else if (path === '/AJAXJSONCrossCORS') {
+        if (Math.random() > 0.5) {
+            response.setHeader('Content-Type', 'text/json')
+            response.setHeader('Access-Control-Allow-Origin','http://client.com:7777')
+            response.statusCode = 200
+            response.write(`
+            {
+                "note":{
+                    "from":"fpc",
+                    "to":"syq",
+                    "body":"i love you"
+                }
+            }
+            `)
+        } else {
+            response.statusCode = 400
+        }
+        response.end()
+    } else {
         response.statusCode = 404
-        response.setHeader('Content-Type','text/html;charset=utf-8')
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write('找不到路径')
         response.end()
     }
